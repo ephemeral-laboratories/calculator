263c015ed103389a75c3ef9d11b0eeb1cb5c12e9
Hakanai <hakanai@ephemeral.garden>
2022-03-05 22:17:23 +1100

    Detect dark theme

--- src/main/kotlin/ui/AppTheme.kt
+++ src/main/kotlin/ui/AppTheme.kt
@@ -1,7 +1,10 @@
 package garden.ephemeral.calculator.ui
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Typography
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.platform.Font
@@ -12,6 +15,7 @@ fun AppTheme(content: @Composable () -> Unit) {
         typography = Typography(
             defaultFontFamily = FontFamily(Font("/RobotoCondensed-Regular.ttf"))
         ),
+        colors = if (isSystemInDarkTheme()) darkColors() else lightColors()
     ) {
         content()
     }
