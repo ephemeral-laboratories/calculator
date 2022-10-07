6bb05f095cd20368bb8f956340cc9b438bae0ef9
Hakanai <hakanai@ephemeral.garden>
2022-10-07 19:36:52 +1100

    Icon on main window

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,18 @@
 package garden.ephemeral.calculator
 
-import androidx.compose.ui.window.singleWindowApplication
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
 import garden.ephemeral.calculator.ui.MainUi
 
-fun main() = singleWindowApplication(
-    title = "Calculator",
-) {
-    MainUi()
+// `singleWindowApplication` doesn't work if I want an icon:
+// https://github.com/JetBrains/compose-jb/issues/2369
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Calculator",
+        icon = painterResource("/AppIcon.ico"),
+    ) {
+        MainUi()
+    }
 }
