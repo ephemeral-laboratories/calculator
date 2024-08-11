f85d660f19afb15845e913e5fc882492aa9dc904
Hakanai <hakanai@ephemeral.garden>
2024-08-12 01:38:26 +1000

    Work around scrollbar not inheriting correct style in material3

--- src/main/kotlin/ui/theme/Theme.kt
+++ src/main/kotlin/ui/theme/Theme.kt
@@ -1,8 +1,11 @@
 package garden.ephemeral.calculator.ui.theme
 
+import androidx.compose.foundation.LocalScrollbarStyle
+import androidx.compose.foundation.defaultScrollbarStyle
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import garden.ephemeral.calculator.ui.ThemeOption
 
 @Composable
@@ -15,9 +18,15 @@ fun AppTheme(option: ThemeOption, content: @Composable () -> Unit) {
         ThemeOption.LIGHT -> LightColors
     }
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = CustomTypography,
-        content = content,
-    )
+    MaterialTheme(colorScheme = colorScheme, typography = CustomTypography) {
+        // Works around a material3 oversight where it doesn't set up the scrollbar style for some reason. Bug?
+        CompositionLocalProvider(
+            LocalScrollbarStyle provides defaultScrollbarStyle().copy(
+                shape = MaterialTheme.shapes.small,
+                unhoverColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f),
+                hoverColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.50f),
+            ),
+            content = content,
+        )
+    }
 }
