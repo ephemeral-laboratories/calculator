f57b88a916c65e084de7661b2b40cf15ae0c2812
Hakanai <hakanai@ephemeral.garden>
2024-12-24 18:04:06 +1100

    Move font into composeResources

--- src/jvmMain/kotlin/ui/theme/Fonts.kt
+++ src/jvmMain/kotlin/ui/theme/Fonts.kt
@@ -1,27 +1,32 @@
 package garden.ephemeral.calculator.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.platform.Font
+import garden.ephemeral.calculator.calculator.generated.resources.Res
+import garden.ephemeral.calculator.calculator.generated.resources.RobotoCondensed_Regular
+import org.jetbrains.compose.resources.Font
 
-val RobotoCondensed = FontFamily(Font("/RobotoCondensed-Regular.ttf"))
-
-internal val DefaultTypography = Typography()
-
-val CustomTypography = DefaultTypography.copy(
-    displayLarge = DefaultTypography.displayLarge.copy(fontFamily = RobotoCondensed),
-    displayMedium = DefaultTypography.displayMedium.copy(fontFamily = RobotoCondensed),
-    displaySmall = DefaultTypography.displaySmall.copy(fontFamily = RobotoCondensed),
-    headlineLarge = DefaultTypography.headlineLarge.copy(fontFamily = RobotoCondensed),
-    headlineMedium = DefaultTypography.headlineMedium.copy(fontFamily = RobotoCondensed),
-    headlineSmall = DefaultTypography.headlineSmall.copy(fontFamily = RobotoCondensed),
-    titleLarge = DefaultTypography.titleLarge.copy(fontFamily = RobotoCondensed),
-    titleMedium = DefaultTypography.titleMedium.copy(fontFamily = RobotoCondensed),
-    titleSmall = DefaultTypography.titleSmall.copy(fontFamily = RobotoCondensed),
-    bodyLarge = DefaultTypography.bodyLarge.copy(fontFamily = RobotoCondensed),
-    bodyMedium = DefaultTypography.bodyMedium.copy(fontFamily = RobotoCondensed),
-    bodySmall = DefaultTypography.bodySmall.copy(fontFamily = RobotoCondensed),
-    labelLarge = DefaultTypography.labelLarge.copy(fontFamily = RobotoCondensed),
-    labelMedium = DefaultTypography.labelMedium.copy(fontFamily = RobotoCondensed),
-    labelSmall = DefaultTypography.labelSmall.copy(fontFamily = RobotoCondensed),
-)
+val CustomTypography: Typography
+    @Composable
+    get() {
+        val defaultTypography = Typography()
+        val robotoCondensed = FontFamily(Font(Res.font.RobotoCondensed_Regular))
+        return defaultTypography.copy(
+            displayLarge = defaultTypography.displayLarge.copy(fontFamily = robotoCondensed),
+            displayMedium = defaultTypography.displayMedium.copy(fontFamily = robotoCondensed),
+            displaySmall = defaultTypography.displaySmall.copy(fontFamily = robotoCondensed),
+            headlineLarge = defaultTypography.headlineLarge.copy(fontFamily = robotoCondensed),
+            headlineMedium = defaultTypography.headlineMedium.copy(fontFamily = robotoCondensed),
+            headlineSmall = defaultTypography.headlineSmall.copy(fontFamily = robotoCondensed),
+            titleLarge = defaultTypography.titleLarge.copy(fontFamily = robotoCondensed),
+            titleMedium = defaultTypography.titleMedium.copy(fontFamily = robotoCondensed),
+            titleSmall = defaultTypography.titleSmall.copy(fontFamily = robotoCondensed),
+            bodyLarge = defaultTypography.bodyLarge.copy(fontFamily = robotoCondensed),
+            bodyMedium = defaultTypography.bodyMedium.copy(fontFamily = robotoCondensed),
+            bodySmall = defaultTypography.bodySmall.copy(fontFamily = robotoCondensed),
+            labelLarge = defaultTypography.labelLarge.copy(fontFamily = robotoCondensed),
+            labelMedium = defaultTypography.labelMedium.copy(fontFamily = robotoCondensed),
+            labelSmall = defaultTypography.labelSmall.copy(fontFamily = robotoCondensed),
+        )
+    }
